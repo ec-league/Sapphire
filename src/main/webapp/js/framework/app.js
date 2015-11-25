97ceef48e8a2e9bb3e80549f886bf5596b81e292
yun.li <yun.li@Ctrip.com>
2015-11-25 18:09:23 +0800

    modify dashboard & add project page

---
+++ src/main/webapp/js/framework/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yun.li on 2015/11/25.
+ */
+//Æô¶¯Ä£¿é
+var app = angular.module('app', [
+    'app.common',
+    'app.service',
+
+
+], function () {
+    console.log('here');
+
+});
+
+app.controller('CommonCtrl',
+    function ($scope) {
+
+    }
+)
+angular.module("app", []);
+angular.module("commonCtrl", []);
+angular.module("app.common", []);
