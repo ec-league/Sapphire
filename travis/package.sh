e32c1a3358f38f45ddeb4208ec7b9264603faa25
baiyp <baiyp@Ctrip.com>
2017-02-09 14:03:11 +0800

    Try fix sonnar error.
    Change-Id: I1ce33d9e5ef4deadad4c2ba8be250147bdd97100

--- travis/package.sh
+++ travis/package.sh
@@ -4,4 +4,6 @@ mvn clean cobertura:cobertura -s settings.xml -Ptravis
 
 bash <(curl -s https://codecov.io/bash)
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -s settings.xml -Dsonar.core.codeCoveragePlugin=jacoco -Dsonar.jacoco.reportPath=tests/jacoco-exec/jacoco.exec -Dsonar.dynamicAnalysis=reuseReports -Dsonar.surefire.reportsPath=tests/test-reports
+mvn clean install -s settings.xml
+
+mvn sonar:sonar -s settings.xml
