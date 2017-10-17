950446a991c43dc77111afada08de5b04d3214a9
sanhuang <yunpeng.byp@antfin.com>
2017-10-17 15:22:52 +0800

    Fix UT error.

--- travis/package.sh
+++ travis/package.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-mvn clean cobertura:cobertura -s settings.xml -Ptravis
+mvn clean install cobertura:cobertura -s settings.xml -Ptravis
 
 bash <(curl -s https://codecov.io/bash)
-
-mvn clean install -s settings.xml
-
-mvn sonar:sonar -s settings.xml
