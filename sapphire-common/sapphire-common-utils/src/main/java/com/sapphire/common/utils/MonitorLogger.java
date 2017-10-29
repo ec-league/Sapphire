0957b93daaee56ae2a08bba23f28a2084f459f02
sanhuang <yunpeng.byp@antfin.com>
2017-10-29 18:36:36 +0800

    Try implement logger

---
+++ sapphire-common/sapphire-common-utils/src/main/java/com/sapphire/common/utils/MonitorLogger.java
@@ -0,0 +1,79 @@
+package com.sapphire.common.utils;
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+
+import java.util.Set;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author yunpeng.byp
+ * @version $Id: com.sapphire.common.utils.MonitorLogger.java, v 0.1 2017年10月29日 下午6:08 yunpeng.byp Exp $
+ */
+public class MonitorLogger implements MethodInterceptor {
+    private String      logName;
+    private Set<String> interceptPackages;
+
+    private Logger      logger;
+
+    public void init() {
+        logger = LoggerFactory.getLogger(logName);
+    }
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+
+        String packageName = invocation.getMethod().getDeclaringClass().getPackage().getName();
+
+        String method = invocation.getMethod().getDeclaringClass().getSimpleName() + "."
+                        + invocation.getMethod().getName();
+
+        if (!interceptPackages.contains(packageName))
+            return invocation.proceed();
+
+        long start = System.currentTimeMillis();
+
+        boolean hasError = false;
+
+        try {
+            return invocation.proceed();
+        } catch (Throwable ex) {
+            hasError = true;
+            throw ex;
+        } finally {
+            if (logger.isInfoEnabled()) {
+                long elapseTime = System.currentTimeMillis() - start;
+
+                if (hasError) {
+                    logger.info(String.format("([%s]:N,%d)", method, elapseTime));
+                } else {
+                    logger.info(String.format("([%s]:Y,%d)", method, elapseTime));
+                }
+            }
+        }
+    }
+
+    /**
+     * Setter method for property <tt>logName</tt>.
+     *
+     * @param logName  value to be assigned to property logName
+     */
+    public void setLogName(String logName) {
+        this.logName = logName;
+    }
+
+    /**
+     * Setter method for property <tt>interceptPackages</tt>.
+     *
+     * @param interceptPackages  value to be assigned to property interceptPackages
+     */
+    public void setInterceptPackages(Set<String> interceptPackages) {
+        this.interceptPackages = interceptPackages;
+    }
+}
