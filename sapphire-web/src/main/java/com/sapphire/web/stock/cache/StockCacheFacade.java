6fdc4581ede62aa32265f876ac634544460ad410
三皇 <yunpeng.byp@antfin.com>
2018-05-12 21:43:06 +0800

    修改url映射

--- sapphire-web/src/main/java/com/sapphire/web/stock/cache/StockCacheFacade.java
+++ sapphire-web/src/main/java/com/sapphire/web/stock/cache/StockCacheFacade.java
@@ -1,11 +1,5 @@
 package com.sapphire.web.stock.cache;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.sapphire.biz.stock.cache.StockCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sapphire.biz.stock.algorithm.action.MacdAction;
-import com.sapphire.biz.stock.algorithm.context.StockContext;
-import com.sapphire.biz.stock.service.StockStatisticsService;
-import com.sapphire.common.dal.stock.domain.StockStatistics;
-import com.sapphire.common.utils.cache.CacheService;
+import com.sapphire.biz.stock.cache.StockCache;
 
 /**
  * @author: Ethan
@@ -30,7 +20,7 @@ public class StockCacheFacade {
 
     private StockCache stockCache;
 
-    @RequestMapping("/refresh")
+    @RequestMapping("/refresh.ep")
     @ResponseBody
     public boolean refresh() {
         return stockCache.refresh();
